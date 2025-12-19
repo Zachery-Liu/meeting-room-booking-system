d7df3118f6c8f6ac70c1b110a942fe48f7134bec
刘梓轩 <liuzixuan@stud.tjut.edu.cn>
2025-12-19 05:58:43 +0000

    提交

---
+++ pages/Meeting_Detail/jsobjects/main/main.js
@@ -0,0 +1,5 @@
+export default {
+  initPage: () => {
+    showAlert("页面初始化成功", "success");
+  }
+}
--- pages/注册页面/jsobjects/JSObject5/JSObject5.js
+++
@@ -1,4 +0,0 @@
-if (checkUsername.data[0].count > 0) {
-  showAlert('用户名已被占用，请更换', 'error');
-  return false;
-}
